10ca7c93f7a6e26edf2a5d0f5585e6d9a65ff7b2
Viyanney Maria Joseph H <viyanney.m.joseph@gmail.com>
2025-11-10 12:28:22 +0530

    Update Deploy.bat

--- Deploy.bat
+++ Deploy.bat
@@ -1,21 +1,20 @@
 @echo off
 echo ======== DEPLOY START ========
-set SRC_DIR=%~dp0dist
-set DEST_DIR=C:\inetpub\wwwroot\MyFrontend
 
-echo Deploying from "%SRC_DIR%" to "%DEST_DIR%"
-if not exist "%DEST_DIR%" (
-  echo Target directory does not exist. Creating it...
-  mkdir "%DEST_DIR%"
-)
+set SRC=%~dp0dist
+set DEST=C:\inetpub\wwwroot\MyFrontend
 
+echo Deploying from "%SRC%" to "%DEST%"
 echo Copying files...
-robocopy "%SRC_DIR%" "%DEST_DIR%" /E /MIR /XD node_modules .git .github
 
-if %errorlevel% LEQ 1 (
-  echo ✅ Deployment successful!
+robocopy "%SRC%" "%DEST%" *.* /S /E /DCOPY:DA /COPY:DAT /PURGE /MIR /R:1000000 /W:30 /XD node_modules .git .github
+
+set RC=%ERRORLEVEL%
+
+if %RC% LEQ 3 (
+  echo ✅ Deployment completed successfully!
   exit /b 0
 ) else (
-  echo ❌ Deployment failed with error code %errorlevel%!
-  exit /b 3
+  echo ❌ Deployment failed with error code %RC%!
+  exit /b %RC%
 )
